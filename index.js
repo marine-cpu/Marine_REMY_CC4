843a06d95d0221687d9f1865611c38ecd934d0a3
marine-cpu <marine.noumea98@gmail.com>
2024-10-22 11:31:25 +1100

    commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const swaggerJsDoc=require('swagger-jsdoc');
 const swaggerUi=require('swagger-ui-express');
-
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+const crypto=require('crypto');
 
 const app = express();
-
-const sqlite3 = require('sqlite3').verbose();
 const db= new sqlite3.Database(process.env.DB_FILE);
 
 // Configuration de Swagger
@@ -26,14 +26,14 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 db.serialize(()=>{
-    db.run("CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY, short_url TEXT, original_url TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY, short_url TEXT, original_url TEXT,visit INTEGER)");
 });
 
-const port = process.env.PORT || 3000;
-
-// Middleware pour parser le corps des requêtes en JSON
-app.use(express.json());
+const port = process.env.PORT || 8080;
 
 /**
  * @swagger
@@ -46,9 +46,18 @@ app.use(express.json());
  *         description: "Succès"
  */
 
+
+
 app.get('/', (req, res) => {
-  res.json({ message: 'Bienvenue à l\'API V1!' });
+    db.get("SELECT COUNT(original_url) AS count FROM urls ",(err,row)=>{
+        if(err){
+            res.status(500).json({message:'Erreur base de donnée',error:err.message});
+        }
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+        return res.json({ linkCount: row.count });
+    });
 });
+
 
 /**
  * @swagger
@@ -70,10 +79,42 @@ app.get('/', (req, res) => {
  *         description: "Données reçues"
  */
 
+
 app.post('/', (req, res) => {
-  const data = req.body;
-  res.json({ message: 'Données reçues!', data });
+  const url = req.body.url;
+  if(!url){
+    return res.status(400).json({ message: 'URL requise.' });
+  }
+
+  db.get("SELECT * FROM urls WHERE original_url=?",[url],(err,row)=>{
+    if(err){
+        res.status(500).json({message:'Erreur serveur',error:err.message});
+    }
+
+    if(row){
+        const newVisitCount=row.visit+1;
+        db.run("UPDATE urls SET visit=? WHERE short_url=?",[newVisitCount,row.short_url],(err)=>{
+            if(err){
+                res.status(500).json({message:'erreur mise à jour'});
+            }else{
+                res.json({ message: `${url}`,visit:newVisitCount, short_url: row.short_url });
+                
+            }
+        });
+    }else{
+        const short_url=crypto.randomBytes(3).toString('hex'); 
+
+        db.run('INSERT INTO urls (short_url, original_url, visit) VALUES (?, ?, ?)', [short_url, url, 1], (err) => {
+          if (err) {
+            return res.status(500).json({ message: 'Erreur lors de l\'insertion de l\'URL', error: err.message });
+          }
+          // Renvoie un message indiquant que l'URL a été ajoutée
+          return res.json({ message: `Url ajoutée : ${short_url}`, visit: 1 });
+        });
+    }
+  });
 });
+
 
 /**
  * @swagger
@@ -115,11 +156,27 @@ app.get('/error', (req, res) => {
  *               properties:
  *                 message:
  *                   type: string
- *                   example: "Vous avez demandé des infos pour : example.com"
+ *                   example: "Url:nomurl"
  */
 app.get('/:url', (req, res) => {
-//   const url = req.params.url;
-//   res.json({ message: `Vous avez demandé des infos pour : ${url}` });
+    const short_url=req.params.url;
+
+    db.get("SELECT * FROM urls WHERE short_url=?",[short_url],(err,row)=>{
+        if(err){
+            return res.status(500).json({ message: 'Erreur serveur', error: err.message });
+        }
+        if(row){
+            const newVisitCount = row.visit + 1;
+            db.run("UPDATE urls SET visit=? WHERE short_url=?", [newVisitCount, short_url], (err) => {
+              if (err) {
+                return res.status(500).json({ message: 'Erreur lors de la mise à jour des visites', error: err.message });
+              }
+              return res.redirect(row.original_url);
+            });
+        }else{
+            return res.status(404).json({message:"Pas d'Url trouvée"})
+        }
+    })
 });
 
 
@@ -166,14 +223,28 @@ app.get('/:url', (req, res) => {
  *                   example: "ECONNREFUSED"
  */
 app.get('/status/:url', async (req, res) => {
-//   const url = req.params.url;
-
-//   try {
-//     const response = await axios.get(`http://${url}`);
-//     res.json({ message: `L'URL est accessible`, status: response.status });
-//   } catch (error) {
-//     res.status(400).json({ message: `Impossible d'accéder à l'URL`, error: error.message });
-//   }
+    const short_url = req.params.url;
+    db.get('SELECT original_url,created_at,visit FROM urls WHERE short_url=?',[url],async (err,row)=>{
+        if(err){
+            return res.status(500).json({ message: 'Erreur serveur', error: err.message });
+        }
+        if(!row){
+            return res.status(404).json({ message: 'URL raccourchie non trouvée.' });
+        }
+        try{
+            const response = await axios.get(row.original_url);
+            return res.json({
+                original_url: row.original_url,
+                created_at: row.created_at,
+                visit: row.visit,
+                message: `L'URL est accessible`,
+                status: response.status
+            });
+        }
+        catch(error){
+            return res.status(400).json({ message: "Impossible d'accéder à l'URL", error: error.message });
+        }
+    });
 });
 
 // Middleware de gestion des erreurs
